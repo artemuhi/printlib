7460442c32e86d965b4f4427139fa978dc277bb3
root <root@noreply.localhost>
2024-02-13 07:06:06 +0300

    Написан "Скелет" библиотеки+константы.

---
+++ printlib.php
@@ -0,0 +1,12 @@
+<?php
+define("MAX_CHAR", 32);
+define("MAX_STR", 32);
+define("ESC", "\x1b");
+define("GS", "\x1d");
+define("LF", "\x0a");
+define("PORT", "/dev/usb/lp0")
+function initprint() {}
+function println($data) {}
+function printbarcode($data, $type="CODE39") {}
+function parse4print($data) {}
+?>

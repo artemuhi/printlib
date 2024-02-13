10f252e182c53cdb49453055d995a945078ad6eb
Artem <artem@noreply.localhost>
2024-02-13 11:33:09 +0300

    Дописана функция

--- printlib.php
+++ printlib.php
@@ -5,7 +5,13 @@ define("ESC", "\x1b");
 define("GS", "\x1d");
 define("LF", "\x0a");
 define("PORT", "/dev/usb/lp0");
-function rawlen($data) {}
+function rawlen($data) {
+    if (strlen($data)<255 and strlen($data)<MAX_CHAR) {
+        return hex2bin(dechex(strlen($data)));
+    } else {
+        return false;
+    }
+}
 function initprint() {
     return file_put_contents(PORT, ESC . "@");
 }
@@ -18,16 +24,20 @@ function println($data) {
 }
 function printcode39($data) {}
 function printcode128($data) {
-    $lp=fopen(PORT, 'w');
-    fwrite($lp, "\x1b@");
-    fwrite($lp, "\x1dh\x50");
-    fwrite($lp, "\x1dH\x02");
-    fwrite($lp, "\x1df\x01");
-    fwrite($lp, "\x1dk\x49");
-    fwrite($lp, rawlen($data))
-    fwrite($lp, $data);
-    fwrite($lp, "\x0a");
-    fclose($lp);
+    if (rawlen($data)) {
+        $lp=fopen(PORT, 'w');
+        fwrite($lp, "\x1dh\x50");
+        fwrite($lp, "\x1dH\x02");
+        fwrite($lp, "\x1df\x01");
+        fwrite($lp, "\x1dk\x49");
+        fwrite($lp, rawlen($data));
+        fwrite($lp, $data);
+        fwrite($lp, "\x0a");
+        fclose($lp);
+        return true;
+    } else {
+        return false;
+    }
 }
 function printean13($data) {}
 function printean8($data) {}
